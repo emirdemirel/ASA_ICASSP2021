faf50038487afc686fd93ae8fbe3d6dc4f3c428e
emir demirel <emir.demirel@upf.edu>
2020-09-15 23:57:41 +0200

    updatedo

--- a2l/run_mirex2020_alignment.sh
+++ a2l/run_mirex2020_alignment.sh
@@ -3,16 +3,22 @@
 # Configuration
 
 nj=1
-stage=0
-model_dir_chain=model/ctdnn
+stage=1
 
-alignment_model=ctdnn_sa    # FOR phoneme based NN model
-               # ctdnn_sa     FOR grapheme-based NN model
-               # gmm_hmm      FOR phoneme-based GMM-HMM model
-               # gmm_hmm_grph FOR grapheme-based GMM-HMM model
+polyphonic=true   #set to false for accapella
+
+decoding_model=ctdnnsa_ivec    # FOR phoneme based NN model with ivectors
+               # ctdnnsa         FOR phoneme based NN model without ivectors
+
+alignment_model=ctdnnsa_ivec    #      FOR phoneme based NN model with ivectors
+               # ctdnnsa               FOR phoneme based NN model without ivectors
+               # ctdnnsa_grph_ivec     FOR grapheme-based NN model with ivectors
+               # gmm_hmm               FOR phoneme-based GMM-HMM model
 
 . ./path.sh
 . ./cmd.sh
+
+. ./utils/parse_options.sh
 
 wavpath=$1
 lyricspath=$2
@@ -24,10 +30,13 @@ rec_id=(${rec_name//$(echo ".$audio_format")/ })
 echo $rec_id
 lang_dir=data/lang_${rec_id}
 
+[[ ! -L "steps" ]] && ln -s $KALDI_ROOT/egs/wsj/s5/steps
+[[ ! -L "utils" ]] && ln -s $KALDI_ROOT/egs/wsj/s5/utils
+
 echo; echo "===== Starting at  $(date +"%D_%T") ====="; echo
 
 outdir_ss=$savepath/audio_vocals #output directory to save the vocals separated audio files.
-if [[ $stage -le 0 ]]; then
+if [[ $polyphonic == true ]]; then
     echo "SOURCE SEPARATION"
     # At this step, we separate vocals. This is required
     # for Vocal-Activity-Detection based initial audio 
@@ -37,7 +46,12 @@ if [[ $stage -le 0 ]]; then
     cd ..
     mv $outdir_ss/demucs/${rec_id}/vocals.wav $outdir_ss/${rec_id}_vocals.wav
     rm -r $outdir_ss/demucs/${rec_id}/  # remove accompiment output as we won't need it.
+else
+    cp $wavpath $outdir_ss/${rec_id}_vocals.wav
+
 fi
+
+###################################
 
 wavpath_vocals=$outdir_ss/${rec_id}_vocals.wav
 if [[ $stage -le 1 ]]; then
@@ -56,6 +70,8 @@ if [[ $stage -le 1 ]]; then
     python3 local/data_preparation.py $lyricspath $wavpath_vocals conf/dict/lexicon_raw.txt data/${rec_id}_vocals 
     ./utils/fix_data_dir.sh data/${rec_id}_vocals
 fi
+
+####################################
 
 
 if [[ $stage -le 2 ]]; then
@@ -84,23 +100,57 @@ if [[ $stage -le 2 ]]; then
     cp -r $lang_dir ${lang_dir}_original
 fi
 
+######################################
 
+model_dir_chain=model/$decoding_model
 if [[ $stage -le 3 ]]; then
-
     echo "AUDIO SEGMENTATION"
     echo "- !!! This process might take a while as it is based on a recursive search algorithm."
-    ./local/run_recursive_segmentation.sh --dataset_id ${rec_id}_vocals \
-      --wavpath_orig $wavpath --wavpath_vocals $wavpath_vocals \
-      --data_orig data/${rec_id} data/${rec_id}_vocals \
-      $model_dir_chain $lang_dir
+    echo
+    if [[ $decoding_model == 'ctdnnsa' ]]; then
+      ./local/run_recursive_segmentation.sh --dataset_id ${rec_id}_vocals \
+        --wavpath_orig $wavpath --wavpath_vocals $wavpath_vocals \
+        --data_orig data/${rec_id} data/${rec_id}_vocals \
+        $model_dir_chain $lang_dir || exit 1
+    elif [[ $decoding_model == 'ctdnnsa_ivec' ]]; then
+      ./local/run_recursive_segmentation_ivec.sh --dataset_id ${rec_id}_vocals \
+        --wavpath_orig $wavpath --wavpath_vocals $wavpath_vocals \
+        --data_orig data/${rec_id} data/${rec_id}_vocals \
+        $model_dir_chain $lang_dir || exit 1
+    fi
 fi
 
+######################################
 
 data_dir_segmented=data/${rec_id}_vocals_vadseg
 data_dir_final=data/${rec_id}_vadseg
 data_id=$(basename -- $data_dir_segmented)
 
-if [[ $alignment_model == 'ctdnn_sa' ]]; then
+if [[ $alignment_model == 'ctdnnsa_ivec' ]]; then
+
+    echo "WORD-LEVEL ALIGNMENT"
+    echo
+    echo "Feature Extraction last time for alignment."
+    echo
+    utils/fix_data_dir.sh $data_dir_segmented
+    steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 --mfcc-config conf/mfcc_hires.conf \
+      $data_dir_segmented exp/make_mfcc/${n}_vadseg mfcc
+    steps/compute_cmvn_stats.sh $data_dir_segmented
+    utils/fix_data_dir.sh $data_dir_segmented
+    steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 1 \
+      ${data_dir_segmented} model/ivector/extractor \
+      model/ivector/ivectors_${data_id}_hires
+    echo "Forced alignment using Phoneme based CTDNN_SA model"
+    echo
+    ali_dir=exp/${rec_id}_vocals/${rec_id}_vocals_ali
+    local/align_chain.sh --cmd "$train_cmd" --nj 1 --beam 100 --retry_beam 7000 \
+      --frames_per_chunk 140 --scale-opts '--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0' \
+      --online_ivector_dir model/ivector/ivectors_${data_id}_hires \
+      $data_dir_segmented $lang_dir $model_dir_chain $ali_dir
+    echo "Generating output files"
+    ./local/generate_output_alignment.sh --frame_shift 0.03 $data_dir_segmented $rec_id ${lang_dir}_original $ali_dir $savepath/alignment
+
+elif [[ $alignment_model == 'ctdnnsa' ]]; then
 
     echo "WORD-LEVEL ALIGNMENT"
     echo
@@ -114,19 +164,15 @@ if [[ $alignment_model == 'ctdnn_sa' ]]; then
     echo "Forced alignment using Phoneme based CTDNN_SA model"
     echo
     ali_dir=exp/${rec_id}_vocals/${rec_id}_vocals_ali
-    local/align_chain.sh --cmd "$train_cmd" --nj 1 --beam 30 --retry_beam 7000 \
+    local/align_chain.sh --cmd "$train_cmd" --nj 1 --beam 100 --retry_beam 7000 \
       --frames_per_chunk 140 --scale-opts '--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0' \
-      $data_dir_segmented $lang_dir $model_dir_chain $ali_dir
+      $data_dir_segmented $lang_dir model/$alignment_model $ali_dir
     echo "Generating output files"
     ./local/generate_output_alignment.sh --frame_shift 0.03 $data_dir_segmented $rec_id ${lang_dir}_original $ali_dir $savepath/alignment
 
-
-fi
-
-if [[ $alignment_model == 'ctdnn_sa_grph' ]]; then
+elif [[ $alignment_model == 'ctdnnsa_grph_ivec' ]]; then
 
     lang_dir_grph=data/lang_${rec_id}_grph
-    model_dir_chain_grph=model/ctdnn_grph
     echo "WORD-LEVEL ALIGNMENT"
     echo
     echo "Create graph for grapheme based model."
@@ -147,18 +193,14 @@ if [[ $alignment_model == 'ctdnn_sa_grph' ]]; then
     echo "Forced alignment using Grapheme based CTDNN_SA model"
     echo
     ali_dir=exp/${rec_id}_vocals/${rec_id}_vocals_ali
-    local/align_chain.sh --cmd "$train_cmd" --nj 1 --beam 50 --retry_beam 700 \
+    local/align_chain.sh --cmd "$train_cmd" --nj 1 --beam 100 --retry_beam 7000 \
       --frames_per_chunk 140 --scale-opts '--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0' \
       --online_ivector_dir model/ivector/ivectors_${data_id}_hires \
-      $data_dir_segmented $lang_dir_grph $model_dir_chain_grph $ali_dir
+      $data_dir_segmented $lang_dir_grph model/$alignment_model $ali_dir
     echo "Generating output files"
     ./local/generate_output_alignment.sh --frame_shift 0.03 $data_dir_segmented $rec_id $lang_dir_grph $ali_dir $savepath/alignment
 
-
-fi
-
-
-if [[ $alignment_model == 'gmm_hmm' ]]; then
+elif [[ $alignment_model == 'gmm_hmm' ]]; then
 
     echo "WORD-LEVEL ALIGNMENT"
     echo
@@ -172,16 +214,17 @@ if [[ $alignment_model == 'gmm_hmm' ]]; then
     echo "Forced alignment using Phoneme based CTDNN_SA model"
     echo
     ali_dir=exp/${rec_id}_vocals/${rec_id}_vocals_ali
-    steps/align_fmllr.sh --cmd "$train_cmd" --nj 1 --beam 50 --retry_beam 7000 \
-      $data_dir_segmented $lang_dir $model_dir_chain $ali_dir
+    steps/align_fmllr.sh --cmd "$train_cmd" --nj 1 --beam 100 --retry_beam 7000 \
+      $data_dir_segmented $lang_dir model/$alignment_model $ali_dir
     echo "Generating output files"
     ./local/generate_output_alignment.sh --frame_shift 0.03 $data_dir_segmented $rec_id $lang_dir $ali_dir $savepath/alignment
 
 
 fi
 
+#####################################
 
-rm -r exp/${rec_id}_vocals/${rec_id}_vocals_segmentation
+#rm -r exp/${rec_id}_vocals/${rec_id}_vocals_segmentation
 cp -r data/${rec_id}_vocals_vadseg $savepath/${rec_id}_vocals_vadseg
 rm -r data/${rec_id}*
 rm -r data/lang_${rec_id}
