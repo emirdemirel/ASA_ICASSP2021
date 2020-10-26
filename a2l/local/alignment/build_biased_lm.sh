a63a9cf37cf0d68829e0b89c2ed429e400862330
emir demirel <emir.demirel@upf.edu>
2020-10-26 16:43:11 +0100

    updating n-gram in biased LM

--- a2l/local/alignment/build_biased_lm.sh
+++ a2l/local/alignment/build_biased_lm.sh
@@ -7,7 +7,7 @@ working_dir=$1
 input_file=$2
 lang_dir=$3
 
-build-lm.sh -i $input_file -o $working_dir/lm.gz -n 4
+build-lm.sh -i $input_file -o $working_dir/lm.gz -n 20
 compile-lm $working_dir/lm.gz -t=yes /dev/stdout | grep -v unk | gzip -c > $working_dir/lm.arpa.gz
 gunzip -c $working_dir/lm.arpa.gz | arpa2fst --disambig-symbol=#0 --read-symbol-table=$lang_dir/words.txt - $lang_dir/G.fst
 
