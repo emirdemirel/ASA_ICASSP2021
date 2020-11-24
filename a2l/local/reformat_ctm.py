0faef6d539100b1f858e26a2b22e90524f6dbdc7
emir demirel <emir.demirel@upf.edu>
2020-11-24 17:03:27 +0100

    removed position dependent phones

--- a2l/local/reformat_ctm.py
+++ a2l/local/reformat_ctm.py
@@ -8,7 +8,7 @@ output_file = sys.argv[2]
 ali = []
 with open(input_file,'r') as r:
     for line in r.readlines():
-        ali.append(line.split(' ')[2] + ' ' + str(float(line.split(' ')[2]) + float(line.split(' ')[3])) + ' ' + line.split(' ')[4])
+        ali.append(line.split(' ')[2] + ' ' + str(float(line.split(' ')[2]) + float(line.split(' ')[3])) + ' ' + line.split(' ')[4].split('_')[0])
     rec_id = line.split('_')[0]    
 
 
